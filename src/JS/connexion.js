a946c430dfdeb88746807a576b27e6243cc299e4
Aboubacar KEITA <aboubacarkeita585@gmail.com>
2020-12-14 13:39:38 +0100

    V-1.0

---
+++ src/JS/connexion.js
@@ -0,0 +1,8 @@
+function formulaire()
+{
+    var titre =document.getElementById('titre')
+    var bouton_connect=document.getElementById('bouton_connect');
+
+    bouton_connect.setAttribute('style', 'display:none');
+    titre.innerHTML='S\'Inscription';
+}
